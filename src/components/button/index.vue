fc9ca05920eb206945e24a46f5afbfc789b8e6ea
Sunbridger <739272884@qq.com>
2020-07-29 14:09:00 +0800

    feat: init

---
+++ src/components/button/index.vue
@@ -0,0 +1,26 @@
+<template>
+
+  <view class="index">
+    <text>{{ title }}</text>
+    <icon size='20' :type='typeicon' v-if="typeicon"></icon>
+    <button @tap="onTap" size='mini' :type='typebtn'>{{title}}</button>
+
+  </view>
+</template>
+
+<script>
+
+
+export default {
+    props: {
+        title: '',
+        typebtn: '',
+        typeicon: ''
+    },
+    methods: {
+        onTap() {
+            console.log(this.title);
+        }
+    }
+}
+</script>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,17 +1,32 @@
 <template>
   <view class="index">
-    <text>{{ msg }}</text>
+      <my-button typeicon="success" typebtn="primary" title="这是成功的tit" ></my-button>
+      <picker mode='selector' :range="selector" @change="onchange">
+            <view className='picker'>
+                当前选择：{{selectorChecked}}
+            </view>
+    </picker>
   </view>
 </template>
 
 <script>
 import './index.scss'
+import MyButton from '../../components/button';
 
 export default {
-  data() {
-    return {
-      msg: 'Hello world!'
+    components: {
+        MyButton
+    },
+    data() {
+        return {
+            selector: ['美国', '中国', '巴西', '日本'],
+            selectorChecked: '美国'
+        }
+    },
+    methods: {
+        onchange(e) {
+            this.selectorChecked = this.selector[e.detail.value]
+        }
     }
-  }
 }
 </script>
